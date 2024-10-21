151ec5a438c6b7a9fa262a0f14630fcb23f04545
Megestus <75190962+Megestus@users.noreply.github.com>
2024-10-22 02:18:26 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,32 +1,16 @@
 import { defineUserConfig } from "vuepress";
-import { getDirname, path } from "vuepress/utils";
+
 import theme from "./theme.js";
 
-const __dirname = getDirname(import.meta.url);
-
 export default defineUserConfig({
-  base: "/HUGweb/",
+  base: "/",
 
   lang: "zh-CN",
-  title: "HUGToolBox",
+  title: "文档演示",
   description: "vuepress-theme-hope 的文档演示",
 
   theme,
 
-  alias: {
-    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
-      __dirname,
-      "./components/BlogHero.vue"
-    ),
-  },
-
   // 和 PWA 一起启用
   // shouldPrefetch: false,
-
-  // 删除这个 plugins 配置，因为 searchPro 已经在 theme.ts 中配置了
-  // plugins: [
-  //   searchProPlugin({
-  //     // 配置选项
-  //   }),
-  // ],
 });
